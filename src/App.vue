d89e68b64627243326867a202dd0f63d2692afe3
李泓睿 <lhr0731us@gmail.com>
2021-10-07 00:37:48 +0800

    。。。。

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,55 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawerLeft" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> 功能 </v-list-item-title>
+          <v-list-item-subtitle> 击穿系统 </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon
+        @click="drawerLeft = !drawerLeft"
+      ></v-app-bar-nav-icon>
+
+      <v-toolbar-title>声学成像模块</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  export default {
+    data () {
+      return {
+        drawerLeft: null,
+        items: [
+          { title: '网关登记', icon: 'mdi-view-dashboard' },
+          { title: '实时监测', icon: 'mdi-image' },
+          { title: '数据', icon: 'mdi-help-box' },
+        ],
+        right: null,
+      }
+    },
   }
-}
-</style>
+</script>
